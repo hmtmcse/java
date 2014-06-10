0f508ee612a79667d2808babaa3c84f26524c3ee
H.M.Touhid Mia <hmtm.cse@gmail.com>
2014-06-10 21:32:58 +0600

    Touhid Mia

---
+++ src/FileReadAndReturnAsString.java
@@ -0,0 +1,47 @@
+import java.io.*;
+
+/**
+ * Created by H.M.Touhid Mia on 6/10/2014.
+ */
+public class FileReadAndReturnAsString {
+
+
+    public static void main(String[] args) {
+        FileReadAndReturnAsString fileReadAndReturnAsString = new FileReadAndReturnAsString();
+        String file = fileReadAndReturnAsString.readAndReturnString("C:\\x.txt");
+        System.out.println(file);
+    }
+
+
+    public String readAndReturnString(String location) {
+
+        /** Is File Exists */
+        File file = new File(location);
+        if (!file.exists()) {
+            System.out.println("File Not Exists");
+            return null;
+        }
+
+
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            DataInputStream dataInputStream = new DataInputStream(fileInputStream);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(dataInputStream));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+                stringBuilder.append(System.getProperty("line.separator"));
+            }
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Exception From File Not Found");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Exception From IO Operation");
+            e.printStackTrace();
+        }
+        return stringBuilder.toString();
+    }
+
+}
