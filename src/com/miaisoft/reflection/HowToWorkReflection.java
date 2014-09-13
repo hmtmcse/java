c80f7d263af77ac0f49fb75b665168e670e9bc6c
Touhid Mia <hmtm.cse@gmail.com>
2014-09-13 08:20:25 +0600

    Touhid Mia

--- src/com/miaisoft/reflection/HowToWorkReflection.java
+++ src/com/miaisoft/reflection/HowToWorkReflection.java
@@ -10,101 +10,112 @@ import java.lang.reflect.Method;
 public class HowToWorkReflection {
 
     public static void main(String[] args) {
-        HowToWorkReflection howToWorkReflaction = new HowToWorkReflection();
-        howToWorkReflaction.getAllDeclaredFields();
+        HowToWorkReflection howToWorkReflection = new HowToWorkReflection();
+        howToWorkReflection.getClassFromString();
 
     }
 
-/**
->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-                                        Experiment With Method
-<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-*/
-    public void getAllMethod(){
+    /**
+     * >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+     * Experiment With Method
+     * <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+     */
+    public void getAllMethod() {
         Method[] methods = ReflectionTest.class.getMethods();
-        for(Method method : methods){
+        for (Method method : methods) {
             System.out.println("method = " + method.getName());
         }
     }
 
-    public void getAllDeclaredMethod(){
+    public void getAllDeclaredMethod() {
         Method[] methods = ReflectionTest.class.getDeclaredMethods();
-        for(Method method : methods){
+        for (Method method : methods) {
             System.out.println("method = " + method.getName());
         }
     }
 
-    public void getConstructor(){
+    public void getConstructor() {
         Constructor[] constructors = ReflectionTest.class.getConstructors();
-        for(Constructor constructor : constructors){
+        for (Constructor constructor : constructors) {
             System.out.println("constructor = " + constructor.getName());
         }
     }
 
 
-/**
->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-                                        Experiment With Fields
-<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-*/
+    /**
+     * >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+     * Experiment With Fields
+     * <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+     */
 
-public void getAllFields(){
-    Field[] fields = ReflectionTest.class.getFields();
-    for(Field field : fields){
-        System.out.println("field = " + field.getName());
+    public void getAllFields() {
+        Field[] fields = ReflectionTest.class.getFields();
+        for (Field field : fields) {
+            System.out.println("field = " + field.getName());
+        }
     }
-}
 
-    public void getAllDeclaredFields(){
+    public void getAllDeclaredFields() {
         Field[] fields = ReflectionTest.class.getDeclaredFields();
-        for(Field field : fields){
+        for (Field field : fields) {
             System.out.println("field = " + field.getName());
         }
     }
 
 
-/**
- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-                                        Experiment With Class
- <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
- */
+    /**
+     * >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+     * Experiment With Class
+     * <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+     */
+
+    public void getAllClassInformation() {
+        Class aClass = ReflectionTest.class;
+
+        System.out.println("Class Full Name = " + aClass.getName());
+        System.out.println("Class Name = " + aClass.getSimpleName());
+        System.out.println("Class Package Name = " + aClass.getPackage());
+
+        Package aPackage = aClass.getPackage();
+        Class superclass = aClass.getSuperclass();
+        Class[] interfaces = aClass.getInterfaces();
+    }
 
 
-/**
- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
- Experiment With Method
- <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
- */
-
-
-/**
- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
- Experiment With Method
- <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
- */
-
-
-
-    public void callMethod(String className, String methodName){
-        Class aliveClass = null;
-
+    public void getClassFromString() {
         try {
-
-
-            aliveClass = Class.forName(className);
-            Object aliveObject = aliveClass.newInstance();
-
-
-
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
+            Class aClass = Class.forName("com.miaisoft.reflection.ReflectionTest");
+            System.out.println("Class Name = " + aClass.getSimpleName());
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-
     }
+
+
+    /**
+     * >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+     * Experiment With Method
+     * <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+     */
+
+    public void methodInvoke() {
+        try {
+            Class aClass = Class.forName("com.miaisoft.reflection.ReflectionTest");
+
+
+
+            System.out.println("Class Name = " + aClass.getSimpleName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+     * Experiment With Method
+     * <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+     */
 
 
 }
