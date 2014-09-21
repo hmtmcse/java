0f5c6d1ac2ccd9648f545299f2a543780798c2fe
Touhid Mia <touhid@bitmascot.com>
2014-09-21 11:39:46 +0600

    Touhid Mia

---
+++ src/com/miaisoft/jsoup/ParseHTMLContent.java
@@ -0,0 +1,21 @@
+package com.miaisoft.jsoup;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+
+/**
+ * Created by touhid on 21/09/2014.
+ */
+public class ParseHTMLContent {
+
+
+    public static void main(String[] args) {
+        String html = "<html><head><title>First parse</title></head><body><p>Parsed HTML into a doc.</p></body></html>";
+        Document doc = Jsoup.parse(html);
+        Element body =  doc.body();
+        System.out.println(body.getElementsByTag("body").toString());
+
+    }
+}
