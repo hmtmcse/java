65751513d4b3e8b805acd9904e23aed8ae4e9cc5
Touhid Mia <touhid@bitmascot.com>
2015-01-08 12:15:10 +0600

    File Shell Command

---
+++ src/com/miaisoft/encryption/ShellCommand.java
@@ -0,0 +1,48 @@
+package com.miaisoft.encryption;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by touhid on 8/01/2015.
+ */
+public class ShellCommand {
+
+    public static void main(String[] args) {
+        run("ll");
+    }
+
+
+    public static void run(String command) {
+        String s = null;
+
+        try {
+            Process p = Runtime.getRuntime().exec("ps -ef");
+
+            BufferedReader stdInput = new BufferedReader(new
+                    InputStreamReader(p.getInputStream()));
+
+            BufferedReader stdError = new BufferedReader(new
+                    InputStreamReader(p.getErrorStream()));
+
+            // read the output from the command
+            System.out.println("Here is the standard output of the command:\n");
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+            }
+
+            // read any errors from the attempted command
+            System.out.println("Here is the standard error of the command (if any):\n");
+            while ((s = stdError.readLine()) != null) {
+                System.out.println(s);
+            }
+
+            System.exit(0);
+        } catch (IOException e) {
+            System.out.println("exception happened - here's what I know: ");
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+}
