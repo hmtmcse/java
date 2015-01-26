559e1aa30457608c9a05a5c288e88207debc8ae3
Touhid Mia <touhid@bitmascot.com>
2015-01-26 11:15:42 +0600

    Touhid

---
+++ hmtmcse/src/com/hmtmcse/basic/ArrayList.java
@@ -0,0 +1,28 @@
+package com.hmtmcse.basic;
+
+import java.util.List;
+
+/**
+ * Created by touhid on 26/01/2015.
+ */
+public class ArrayList {
+
+
+    public static void main(String args[]) {
+        List<String> content = new java.util.ArrayList<String>();
+        content.add("A");
+        content.add("B");
+        content.add("C");
+        content.add("D");
+        System.out.println(content.size());
+        content.add("E");
+        content.add("F");
+        System.out.println(content.size());
+
+    }
+
+
+
+
+
+}
