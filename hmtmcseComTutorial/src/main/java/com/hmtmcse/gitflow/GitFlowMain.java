4eb6dff96a4b0e06ebbfbd778c898219fa695c32
Touhid Mia <touhid@bitmascot.com>
2018-07-16 18:02:45 +0600

    Git Command

--- hmtmcseComTutorial/src/main/java/com/hmtmcse/gitflow/GitFlowMain.java
+++ hmtmcseComTutorial/src/main/java/com/hmtmcse/gitflow/GitFlowMain.java
@@ -2,8 +2,46 @@ package com.hmtmcse.gitflow;
 
 public class GitFlowMain {
 
+
+    static String repository = "F:\\touhid\\GitExperiment\\git_experiment";
+
+    public static void git(String command){
+        GitCommandRunner.git("git " + command, repository);
+    }
+
+
+    public static void checkout(String branchName){
+        git("checkout " + branchName);
+    }
+
+    public static void createBranch(String branchName){
+        git("checkout -b " + branchName);
+    }
+
+    public static void rebasePull(){
+        git("pull --rebase");
+    }
+
+
+    public static void rebase(String branch){
+        git("rebase " + branch);
+    }
+
+
+    public static void cleanHistory(){
+        git("checkout --orphan backup_branch");
+        git("add -A");
+        git("commit -am \"bismillah\"");
+        git("branch -D master");
+        git("branch -m master");
+        git("push -f origin master");
+        git("branch --set-upstream-to=origin/master master");
+        git("pull");
+    }
+
+
     public static void main(String[] args) {
-        GitCommandRunner.git("git branch -rf", "F:\\touhid\\GitExperiment\\git_experiment");
+        cleanHistory();
     }
 
 
