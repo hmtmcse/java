6b763de8da762c14d60dd15ac5a12c467f5a9e76
Touhid Mia <hmtm.cse@gmail.com>
2018-09-15 06:52:25 +0600

    Jsoup

---
+++ hmtmcseComTutorial/src/main/java/com/hmtmcse/TableGenerator.java
@@ -0,0 +1,29 @@
+package com.hmtmcse;
+
+import de.vandermeer.asciitable.AT_Row;
+import de.vandermeer.asciitable.AsciiTable;
+import de.vandermeer.asciitable.CWC_LongestWord;
+import org.apache.commons.lang3.ArrayUtils;
+
+import static junit.framework.Assert.assertEquals;
+
+public class TableGenerator {
+
+
+    public static void main(String[] args) {
+        AsciiTable at = new AsciiTable();
+        at.addRule();
+        at.addRow("row 1 col 1", "row 1 col 2");
+        at.addRule();
+        at.addRow("row 2 col 1", "row 2 col 2");
+        at.addRule();
+
+        String rend = at.render();
+        System.out.println("\033[31m" + rend);
+        System.out.println("\033[40m" + "Touhid mia");
+
+
+
+    }
+
+}
---
+++ hmtmcseComTutorial/src/main/java/com/hmtmcse/color/ColorTest.java
@@ -0,0 +1,25 @@
+package com.hmtmcse.color;
+
+public class ColorTest {
+
+    public static void main(String[] args) {
+
+
+        System.out.println( ColorCodes.BLACK_BACKGROUND_BRIGHT + "" + ColorCodes.BLACK_BOLD + "111111111aaaaaaaaaaaaaaaa" + ColorCodes.RESET);
+
+        System.out.println("+--------------------------+-----------+--------------------------+-------------------+");
+        System.out.println(ColorCodes.BLACK_BACKGROUND_BRIGHT + "" + ColorCodes.BLACK_BOLD + "| Touhid Mia               | Team Lead | System & Research        | System & Research |" + ColorCodes.RESET);
+        System.out.println("+--------------------------+-----------+--------------------------+-------------------+");
+
+
+
+//        System.out.print(ColorCodes.CYAN_BACKGROUND);
+//        System.out.print(ColorCodes.RED_BRIGHT);
+//
+//        System.out.print(ColorCodes.RESET);
+//        System.out.println("111111111aaaaaaaaaaaaaaaa==============马哥私房菜");
+//        System.out.println("111111111aaaaaaaaaaaaaaaa==============马哥私房菜");
+//        System.out.println("111111111aaaaaaaaaaaaaaaa==============马哥私房菜");
+//        System.out.print(ColorCodes.RESET);
+    }
+}
